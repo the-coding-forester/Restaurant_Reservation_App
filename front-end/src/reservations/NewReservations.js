c62edb90442de93e57efedf981ec5f68ef7e4095
Stephanie Moore <smoore_forestry@outlook.com>
2021-07-26 21:55:46 -0400

    New Reservations page first draft

---
+++ front-end/src/reservations/NewReservations.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import ReservationForm from "./ReservationsForm";
+
+
+function NewReservationPage() {
+  const history = useHistory
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [reservationDate, setReservationDate] = useState(`${currentDay}`);
+  const [reservationTime, setReservationTime] = useState(`${currentTime}`)
+  const [partySize, setPartySize] = useState(1);
+
+
+  const handleCreateReservation = (event) => {
+    event.preventDefault();
+    setFirstName("");
+    setLastName("");
+    setMobileNumber("");
+    setReservationDate(`${currentDay}`);
+    setReservationTime(`${currentTime}`)
+    setPartySize(1);
+    history.push("/");
+  }
+
+  const handleCancelReservation = () => {
+    setFirstName("");
+    setLastName("");
+    setMobileNumber("");
+    setReservationDate(`${currentDay}`);
+    setReservationTime(`${currentTime}`)
+    setPartySize(1);
+    history.push("/");
+  }
+
+  return (
+    <div className="container">
+      <Menu /> {/*not made yet*/}
+
+      <h1>New Reservations</h1>
+
+      <ReservationForm
+        firstName={firstName}
+        lastName={lastName}
+        mobileNumber={mobileNumber}
+        reservationDate={reservationDate}
+        reservationTime={reservationTime}
+        partySize={partySize}
+        onCancel={handleCancelReservation}
+        onSubmit={handleCreateReservation}
+      />
+
+    </div>
+  )
+}

b52d959e8775ed6b0b7f12bf397e18b817289c64
Stephanie Moore <smoore_forestry@outlook.com>
2021-07-27 16:28:59 -0400

    update layout of application

--- front-end/src/layout/Layout.js
+++ front-end/src/layout/Layout.js
@@ -13,14 +13,10 @@ import "./Layout.css";
  */
 function Layout() {
   return (
-    <div className="container-fluid">
-      <div className="row h-100">
-        <div className="col-md-2 side-bar">
-          <Menu />
-        </div>
-        <div className="col">
-          <Routes />
-        </div>
+    <div className="">
+      <Menu />
+      <div className="col container-fluid">
+        <Routes />
       </div>
     </div>
   );

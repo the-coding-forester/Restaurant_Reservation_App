cd8147dda4a509909a76fb929aa6bc183bf66564
Stephanie Moore <smoore_forestry@outlook.com>
2021-07-28 11:49:27 -0400

    minor update

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -12,18 +12,27 @@ function create(reservation) {
 function read(reservation_id) {
   return knex("reservations")
     .select("*")
-    .where({ reservation_id })
-    .then((reservation) => reservation[0]);
+    .where({ reservation_id: reservation_id })
+    .first();
 }
 
 // List reservations
 function list() {
-  return knex("reservations");
+  return knex("reservations")
+    .select("*");
 }
 
+// List reservations on date
+function listReservationsOnDay(reservation_date) {
+  return knex("reservations")
+    .select("*")
+    .where({ reservation_date: reservation_date })
+    .orderBy("reservation_time")
+}
 
 module.exports = {
   create,
   read,
   list,
+  listReservationsOnDay,
 };

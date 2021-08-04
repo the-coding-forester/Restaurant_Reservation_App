859745197693db7916f3a06b5192bf660698bd9a
Stephanie Moore <smoore_forestry@outlook.com>
2021-08-04 14:27:28 -0400

    udated migrations & reran

--- back-end/src/db/migrations/20210801211749_createTablesTable.js
+++ back-end/src/db/migrations/20210801211749_createTablesTable.js
@@ -2,13 +2,15 @@ exports.up = function (knex) {
   return knex.schema.createTable("tables", (table) => {
     table.increments("table_id").primary();
     table.string("table_name").notNullable();
-    table.integer("capacity").notNullable();
+    table.integer("capacity").defaultTo(1).notNullable();
+    table.boolean("occupied").defaultTo(false).notNullable();
     table.integer("reservation_id").unsigned().unique();
     table
       .foreign("reservation_id")
       .references("reservation_id")
       .inTable("reservations")
       .onDelete("cascade");
+    table.timestamps(true, true);
   });
 };
 

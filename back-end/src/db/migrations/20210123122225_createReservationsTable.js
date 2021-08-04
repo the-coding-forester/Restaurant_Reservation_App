73994922c3b5be8d155d2f536e6f57efa1545d58
Stephanie Moore <smoore_forestry@outlook.com>
2021-08-04 14:27:12 -0400

    udated migrations & reran

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -5,7 +5,7 @@ exports.up = function (knex) {
     table.string("last_name");
     table.string("mobile_number");
     table.date("reservation_date");
-    table.time("reservation_time", { useTz: false });
+    table.time("reservation_time");
     table.integer("people");
     table.string("status");
     table.timestamps(true, true);

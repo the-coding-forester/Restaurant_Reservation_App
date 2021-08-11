083288b255e5724bec8e4943c40c1c13fac7e4aa
Stephanie Moore <smoore_forestry@outlook.com>
2021-08-11 15:16:46 -0400

    minor update

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -15,7 +15,7 @@ function Menu() {
   return (
     <nav className="sticky-top shadow-lg navbar navbar-dark mb-2 px-2 blur-behind">
       <Link to="/" className="navbar-brand">
-        <span className="brand"> Reservations</span>
+        <span className="brand"> Periodic Tables Reservations</span>
       </Link>
       <button
         className="navbar-toggler ml-auto"
